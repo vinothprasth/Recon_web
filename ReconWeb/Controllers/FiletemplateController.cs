957e3e8cb264ee412345c7dbe2e4748d0cca52ed
Hema Jayaraman <hema@flexicodeindia.com>
2023-08-31 13:22:52 +0530

    temp module

---
+++ ReconWeb/Controllers/FiletemplateController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReconWeb.Controllers
+{
+    public class FiletemplateController : Controller
+    {
+        public IActionResult Filetemplate()
+        {
+            return View();
+        }
+
+        public IActionResult Filetemplatedetails()
+        {
+            return View();
+        }
+    }
+}
